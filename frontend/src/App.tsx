3588339be76d7566d7febb4e00439ed60a9cbb44
Zeshan Shahid <z.shahid@hotmail.de>
2023-07-08 12:06:38 +0200

    make a request from frontend with axios and display data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,35 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {Product} from "./Product";
 
 function App() {
+    const [products, setProducts] = useState<Product[]>([]);
 
-  return (
-    <>
-        "Hello World"
-    </>
-  )
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    function getProducts() {
+        axios
+            .get("/api/product")
+            .then(response => {
+                const responseData = response.data as Product[]
+                setProducts(responseData);
+            })
+            .catch(error => {
+                console.error("Error fetching products:", error);
+            });
+    }
+
+    return (
+        <>
+            <h1>Kitchen Storage</h1>
+            <h3>Product List:</h3>
+            {products.map(product => (
+                <div key={product.id}>{product.productName}</div>
+            ))}
+        </>
+    );
 }
 
-export default App
+export default App;
