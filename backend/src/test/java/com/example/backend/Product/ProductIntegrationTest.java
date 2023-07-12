0fd8627aae5a3073c234e64928a276bb39a2eb14
Zeshan Shahid <z.shahid@hotmail.de>
2023-07-12 12:27:42 +0200

    add DirtiesContext annotation

--- backend/src/test/java/com/example/backend/Product/ProductIntegrationTest.java
+++ backend/src/test/java/com/example/backend/Product/ProductIntegrationTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -27,6 +28,7 @@ class ProductIntegrationTest {
                         """));
     }
     @Test
+    @DirtiesContext
     void addProductAndExpectStatus200AndAddedProduct() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
                 .contentType(MediaType.APPLICATION_JSON)
